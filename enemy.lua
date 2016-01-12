481af076c63d5475c04591367eb73e8d1152ecc7
becauseimgray <otakugray@gmail.com>
2016-01-11 17:02:34 -0700

    Fixed some things with the enemy pausing and moving off screen

--- enemy.lua
+++ enemy.lua
@@ -8,12 +8,13 @@ function enemy.load(dt)
   enemy.friction = .9
   enemy.w        = 32
   enemy.h        = 32
-  enemy.x        = window_w / 2
+  enemy.x        = window_w - 50
   enemy.y        = -enemy.h
   enemy.speed    = 90 - enemy.friction
   enemy.img      = love.graphics.newImage("images/enemy.png")
   enemy.count    = 0
   enemy.pause    = false
+  enemy.done     = false
   wait_time      = 2 --changing this changes how long this enemy waits for before moving past whatever boundary I've set
 
   initTime       = love.timer.getTime()
@@ -28,17 +29,22 @@ function enemy.physics(dt)
       enemy.pause = true
       if currentNumber < 0 then
           enemy.pause = false
+          enemy.done = true
+          --enemy.speed = 180 - enemy.friction
         end
     end
 
-  if enemy.count > 50 and enemy.pause ~= true then
+  if enemy.count > 50 and enemy.pause ~= true and enemy.done ~= true then
     enemy.y = enemy.y + (enemy.speed * dt)
+  elseif enemy.done then
+    enemy.x = enemy.x + (enemy.speed * dt)
   end
 
   if enemy.y > window_h then
     table.remove(enemy, 1)
   end
 end
+
 
 function enemy.update(dt)
 
